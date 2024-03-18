80cf9b583929ccfe16d655406ea6d2fa4ff52ffb
davikrig-ha <davi.envolti@malwee.com.br>
2024-03-17 22:31:15 -0300

    add form

--- test-front/src/app/app.module.ts
+++ test-front/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RequesterComponent } from './components/requester/requester.component';
 import { HomeComponent } from './components/home/home.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [AppComponent, RequesterComponent, HomeComponent],
@@ -16,6 +18,9 @@ import { HomeComponent } from './components/home/home.component';
     FormsModule,
     BrowserAnimationsModule,
     MatButtonModule, 
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- test-front/src/app/components/requester/requester.component.ts
+++ test-front/src/app/components/requester/requester.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-requester',
   templateUrl: './requester.component.html',
-  styleUrl: './requester.component.scss'
+  styleUrls: ['./requester.component.scss']
 })
 export class RequesterComponent {
+  requesterForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.requesterForm = this.formBuilder.group({
+      requesterName: ['', Validators.required],
+      description: ['', Validators.required],
+      productPrice: ['', [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.requesterForm.valid) {
+      console.log(this.requesterForm.value);
+    }
+  }
 }
